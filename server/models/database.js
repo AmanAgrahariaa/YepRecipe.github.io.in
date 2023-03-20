5c4bc8e51757a301d337fe4eee8f9e42d2de99c9
aman <agraharia329@gmail.com>
2023-03-20 21:19:50 +0530

    YepRecipe.github.io.in
    First commit

---
+++ server/models/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Connection = ()=>{
+     mongoose.set('strictQuery', true);
+
+  mongoose.connect("mongodb://localhost:27017/RecipeDatabase",{useUnifiedTopology : true,useNewUrlParser: true})
+  .then(()=>console.log("connection successfull .."))
+  .catch((err)=>console.log(err));
+
+}
+Connection();
+
+// Models
+require('./Category');
+require('./recipe');
