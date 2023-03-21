4e6c70e17746020430aef29f0ad28fa45b3010a0
Aman Agrahari <104276710+AmanAgrahariaa@users.noreply.github.com>
2023-03-21 09:45:50 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Connection = ()=>{
      mongoose.set('strictQuery', true);
 
-  mongoose.connect("mongodb://localhost:27017/RecipeDatabase",{useUnifiedTopology : true,useNewUrlParser: true})
+  mongoose.connect("mongodb://0.0.0.0:27017/RecipeDatabase",{useUnifiedTopology : true,useNewUrlParser: true})
   .then(()=>console.log("connection successfull .."))
   .catch((err)=>console.log(err));
 
